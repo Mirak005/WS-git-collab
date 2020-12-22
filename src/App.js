74bffc03377120bd800eec35f286a0d94f7a2cab
Mirak005 <karimgharbbi@gmail.com>
2020-12-22 16:34:48 +0100

    import borhen2

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import React from "react";
-
+import Borhen2 from "./components/Borhen2";
 import "./App.css";
 
 const App = () => {
-  return <div>HELLO WORLD</div>;
+  return (
+    <div>
+      <Borhen2 />
+    </div>
+  );
 };
 
 export default App;
